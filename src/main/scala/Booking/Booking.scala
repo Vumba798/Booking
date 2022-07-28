3fca559f531f7d9c03591c65b1265ba412b683ed
Vumba798 <alexandrov32649@gmail.com>
2022-07-28 23:36:42 +0300

    started implementing Booking methods

--- src/main/scala/Booking/Booking.scala
+++ src/main/scala/Booking/Booking.scala
@@ -1,5 +1,38 @@
 package Booking
 
-class Booking {
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import org.mongodb.scala.model.Filters.equal
+import server.Database.Database
+import server.database.model._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+// Actor that handles Booking API
+object Booking {
+  // TODO change
+  implicit val ec: ExecutionContext = ???
+
+
+
+  def getAvailableTime(startT: String,
+    finishT: String,
+    companyId: Int,
+    master: String): Future[Seq[BookingRecord]] = Database.bookings
+      .find(equal("startT", startT))
+      .toFuture()
+      .recoverWith(e => Future.failed(e))
+
+  def createBooking(price: Double,
+  startT: String,
+  finishT: String,
+  companyId: Int,
+  master: String) = ???
+    /* todo implement
+  Database.bookings
+      .insertOne(BookingRecord(startT, finishT, companyId, master))
+
+     */
+
 
 }
