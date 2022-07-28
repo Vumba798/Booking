89c4157281105b876145e70b932370f045684fc8
Vumba798 <alexandrov32649@gmail.com>
2022-07-28 23:42:21 +0300

    added 2 of 3 providers for deserialization database documents

--- src/main/scala/server/database/Database.scala
+++ src/main/scala/server/database/Database.scala
@@ -1,13 +1,31 @@
 package server.database
 
+import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
+import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
+import org.mongodb.scala.bson.codecs.Macros._
 import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
+import server.database.model._
 
 
 object Database {
   val client: MongoClient = MongoClient()
-  val db: MongoDatabase = client.getDatabase("BookingPractice")
 
+  // todo add provider classOf[Company]
+  val db: MongoDatabase = client.getDatabase("BookingPractice")
+    .withCodecRegistry(fromRegistries(fromProviders(classOf[User], classOf[BookingRecord]), DEFAULT_CODEC_REGISTRY))
+
+  // todo change companies generic type from "Document" to "Company"
   val companies: MongoCollection[Document] = db.getCollection("Companies")
-  val users: MongoCollection[Document] = db.getCollection("Users")
-  val bookings: MongoCollection[Document] = db.getCollection("Bookings")
+  val users: MongoCollection[User] = db.getCollection("Users")
+  val bookings: MongoCollection[BookingRecord] = db.getCollection("Bookings")
 }
+
+
+
+
+
+
+
+
+
+
