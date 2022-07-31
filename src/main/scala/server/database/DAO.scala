3314b27b93666fc5b985949c863a271db7cfee0a
Vumba798 <alexandrov32649@gmail.com>
2022-07-31 14:15:42 +0300

    renamed Database to DAO, added toJson() function

--- src/main/scala/server/database/DAO.scala
+++ src/main/scala/server/database/DAO.scala
@@ -1,5 +1,7 @@
 package server.database
 
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
 import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
 import org.mongodb.scala.bson.codecs.Macros._
@@ -7,26 +9,23 @@ import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
 import server.database.model._
 
 
-object Database {
-  val client: MongoClient = MongoClient()
+object DAO {
+  private val mapper = JsonMapper.builder()
+    .addModule(DefaultScalaModule)
+    .build()
+
+  private val client: MongoClient = MongoClient()
 
   // todo add provider classOf[Company]
-  val db: MongoDatabase = client.getDatabase("BookingPractice")
-    .withCodecRegistry(fromRegistries(fromProviders(classOf[User], classOf[BookingRecord]), DEFAULT_CODEC_REGISTRY))
+  private val db: MongoDatabase =
+  client.getDatabase("BookingPractice").withCodecRegistry(
+    fromRegistries(fromProviders(classOf[User], classOf[BookingRecord]),
+    DEFAULT_CODEC_REGISTRY))
 
   // todo change companies generic type from "Document" to "Company"
   val companies: MongoCollection[Document] = db.getCollection("Companies")
   val users: MongoCollection[User] = db.getCollection("Users")
   val bookings: MongoCollection[BookingRecord] = db.getCollection("Bookings")
+
+  def toJson(value: Any): String = mapper.writeValueAsString(value)
 }
-
-
-
-
-
-
-
-
-
-
-
