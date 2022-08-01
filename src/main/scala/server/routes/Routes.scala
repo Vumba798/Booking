2b2442e9ec6740880c855e007fec399af9a2f2ef
Vumba798 <alexandrov32649@gmail.com>
2022-08-01 22:15:37 +0300

    refactored Routes structure, added implicit ActorSystem and pool of actors references

--- src/main/scala/server/routes/Routes.scala
+++ src/main/scala/server/routes/Routes.scala
@@ -1,33 +1,25 @@
 package server.routes
 
+import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.http.scaladsl.server.Directives.{concat, pathPrefix}
 import akka.http.scaladsl.server.Route
-import server.routes.BookingRoutes.{createBooking, editBooking, getAvailableTime, getBookings, getCompanyBookings}
+import server.database.DatabaseActor
+import server.routes.DbRoutesTrait
+
+final class Routes
+  (override protected val dbActors: ActorRef[DatabaseActor.Command])
+  (override protected implicit val system: ActorSystem[Nothing]) extends DbRoutesTrait {
+
+  private val bookingRoutes = new BookingRoutes(dbActors).routes
+  private val authRoutes = ??? // TODO provide implementation
+  private val analyticsRoutes = ??? // TODO provide implementation
 
 
-object Routes {
-  val routes: Route = pathPrefix("api") {
-    pathPrefix("booking") {
-      concat(
-        getAvailableTime,
-        createBooking,
-        getBookings,
-        editBooking,
-        getCompanyBookings
-      )
-    }
-    // todo provide implementation
-    /*
-    pathPrefix("auth") {
-      registration ~ // api/auth/registration
-      login ~ // api/auth/login
-      invite // api/auth/invite)
-    } ~
-    pathPrefix("analytics") {
-      getAllClients ~ // api/analytics/get_all
-      getLeaderBoard ~ // api/analytics/get_leader
-      getBusiestTimes // api/analytics/get_best_time
-    }
-     */
+  lazy val routes: Route = pathPrefix("api") {
+    concat(
+      authRoutes,
+      bookingRoutes,
+      analyticsRoutes,
+    )
   }
 }
