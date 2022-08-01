ced678a2d4d3bf82d556b021110ca057da87930a
Vumba798 <alexandrov32649@gmail.com>
2022-08-01 22:13:42 +0300

    Added guardian actor responsible for creating a pool of DatabaseActor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,37 @@
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.{Behaviors, Routers}
 import akka.http.scaladsl.Http
-import server.routes.Routes.routes
+import server.database.DatabaseActor
+import server.routes.Routes
+
 import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
 
-object Main extends App {
-  // TODO replace hardcoded values
-  val host = "localhost"
-  val port = 8080
+object Main {
 
-  implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "booking_system")
-  implicit val ec: ExecutionContextExecutor = system.executionContext
+  def apply(): Behavior[Unit] = Behaviors.setup { ctx =>
 
-  val bindingFuture = Http().newServerAt(host, port).bind(routes)
+    implicit val system: ActorSystem[Nothing] = ctx.system
+    // TODO replace hardcoded values
+    val host = "localhost"
+    val port = 8080
+
+    // creating pool of DatabaseActor
+    val poolSize = 4
+    val pool = Routers.pool(poolSize)(DatabaseActor())
+    val dbActors = ctx.spawn(pool, "worker-pool")
+
+    // initializes our routes with giving them ActorRef[DatabaseActor]
+    val routes = new Routes(dbActors).routes
+    val bindingFuture = Http().newServerAt(host, port).bind(routes)
+
+    Behaviors.empty
+  }
+
+  def main(args: Array[String]): Unit = {
+    ActorSystem(Main(), "booking-system")
+    // TODO handle server stopping
+    StdIn.readLine()
+  }
 
 }
