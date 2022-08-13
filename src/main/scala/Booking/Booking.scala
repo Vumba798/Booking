0a148b62c89f63fda88da452926f1eb4c49d6a78
Vumba798 <alexandrov32649@gmail.com>
2022-08-13 16:06:28 +0300

    added createWorkingSchedule api method

--- src/main/scala/Booking/Booking.scala
+++ src/main/scala/Booking/Booking.scala
@@ -2,12 +2,13 @@ package Booking
 
 import org.bson.types.ObjectId
 import org.mongodb.scala.bson.collection.immutable.Document
-import org.mongodb.scala.model.Filters.{gte, lte}
-import org.mongodb.scala.result.InsertOneResult
+import org.mongodb.scala.model.Filters.{gt, gte, lt, lte}
+import org.mongodb.scala.result.{InsertManyResult, InsertOneResult}
 import org.mongodb.scala.model.Filters
 import org.mongodb.scala.model.Filters.equal
 import server.database.Dao
 import server.database.model._
+import server.database.model.requests.CreateWorkingScheduleRequest
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -115,6 +116,69 @@ object Booking {
       )
       .toFuture()
       .recoverWith(e => Future.failed(e))
+  }
+
+  def createWorkingSchedule(
+      params: CreateWorkingScheduleRequest
+  )(implicit
+      ec: ExecutionContext
+  ): Future[InsertManyResult] = {
+
+    val schedule = params.timeslots.map { slot =>
+      BookingRecord.free(
+        companyId = new ObjectId(params.companyId),
+        masterId = new ObjectId(slot.masterId),
+        price = slot.price,
+        startT = slot.startT,
+        finishT = slot.finishT
+      )
+    }
+
+    def hasIntersections = {
+      val listOfFutures = params.timeslots
+        .map { slot =>
+          Dao.bookings
+            .find(
+              Filters.and(
+                equal("companyId", new ObjectId(params.companyId)),
+                equal("masterId", new ObjectId(slot.masterId)),
+                Filters.or(
+                  Filters.and(
+                    gte("startT", slot.startT),
+                    lt("finishT", slot.startT)
+                  ),
+                  Filters.and(
+                    gt("startT", slot.finishT),
+                    lte("finishT", slot.finishT)
+                  )
+                )
+              )
+            )
+            .headOption()
+            .recoverWith(e => Future.failed(e))
+            .map {
+              case None    => false
+              case Some(_) => true
+            }
+        }
+
+      Future
+        .sequence(listOfFutures)
+        .map(_.contains(true))
+    }
+
+    hasIntersections.flatMap {
+      case true =>
+        Future.failed(
+          new IllegalArgumentException("New schedule intersects with old one!")
+        )
+      case false =>
+        Dao.bookings
+          .insertMany(schedule)
+          .toFuture()
+          .recoverWith(e => Future.failed(e))
+    }
+
   }
 
 }
