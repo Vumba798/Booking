b73be3f7a41a2e17a4952067d4eb9f16efd9a1af
Vumba798 <alexandrov32649@gmail.com>
2022-08-18 22:37:04 +0300

    added half-implemented global exception handler

--- src/main/scala/server/routes/Routes.scala
+++ src/main/scala/server/routes/Routes.scala
@@ -1,9 +1,10 @@
 package server.routes
 
 import akka.actor.typed.{ActorRef, ActorSystem}
-import akka.http.scaladsl.server.Directives.{concat, pathPrefix}
-import akka.http.scaladsl.server.Route
-import server.database.DatabaseActor
+import akka.http.scaladsl.server.Directives.{complete, concat, pathPrefix}
+import akka.http.scaladsl.server.{ExceptionHandler, Route}
+import server.actors.database_actor.DatabaseActor
+import server.exceptions.{InsertionException, IntersectionException}
 import server.routes.DbRoutesTrait
 
 final class Routes(
@@ -11,6 +12,21 @@ final class Routes(
 )(
     override protected implicit val system: ActorSystem[Nothing]
 ) extends DbRoutesTrait {
+
+  /* TODO implement ExceptionHandler
+  implicit def exceptionHandler =
+    ExceptionHandler {
+      case e: InsertionException =>
+        complete(???)
+      case e: IntersectionException =>
+        complete(???)
+      case e: RuntimeException =>
+        complete(???)
+      case _ =>
+        complete(???)
+    }
+
+   */
 
   private val bookingRoutes = new BookingRoutes(dbActors).routes
   /*
