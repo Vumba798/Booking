9a8f3e4733e0876f9076660b2487d590eb74c4eb
Vumba798 <alexandrov32649@gmail.com>
2022-08-18 22:21:09 +0300

    refactored routes' parameters

--- src/main/scala/server/routes/BookingRoutes.scala
+++ src/main/scala/server/routes/BookingRoutes.scala
@@ -6,29 +6,19 @@ import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCode}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.util.Timeout
-import server.database.DatabaseActor
-import server.database.DatabaseActor.{
-  CreateBookingCommand,
-  EditBookingCommand,
-  GetAvailableTimeCommand,
-  GetBookingsCommand,
-  GetCompanyBookingsCommand,
-  JsonResponse,
-  StatusCodeResponse
-}
-import server.database.model.requests.{
-  CreateBookingRequest,
-  CreateWorkingScheduleRequest
-}
+import server.actors.database_actor._
+import server.actors.database_actor.commands._
+import server.request_params.booking._
 
 import scala.concurrent.duration.DurationInt
 
 class BookingRoutes(
     override protected val dbActors: ActorRef[DatabaseActor.Command]
-)(override protected implicit val system: ActorSystem[Nothing])
-    extends DbRoutesTrait {
+)(
+    override protected implicit val system: ActorSystem[Nothing]
+) extends DbRoutesTrait {
 
-  implicit val timeout: Timeout = 5.seconds
+  private implicit val timeout: Timeout = 5.seconds
 
   lazy val routes: Route = pathPrefix("booking") {
     concat(
@@ -36,7 +26,8 @@ class BookingRoutes(
       createBooking,
       getBookings,
       editBooking,
-      getCompanyBookings
+      getCompanyBookings,
+      createWorkingSchedule
     )
   }
 
@@ -46,37 +37,29 @@ class BookingRoutes(
         "startT".as[String],
         "finishT".as[String],
         "companyId".as[String],
-        "master".as[String]
-      ) { (startT, finishT, companyId, master) =>
-        val dbResponse = dbActors.ask(ref =>
-          GetAvailableTimeCommand(ref, startT, finishT, companyId, master)
-        )
+        "masterId".as[String]
+      )
+        .as(GetAvailableTimeParams) { params =>
+          val dbResponse =
+            dbActors.ask(ref => GetAvailableTimeCommand(ref, params))
 
-        // TODO add exception handler (onFailure) or replace "onSuccess" with onComplete to handle failures manually
-        onSuccess(dbResponse) {
-          case JsonResponse(json) =>
-            complete(HttpEntity(ContentTypes.`application/json`, json))
-          case _ => complete("undefined response")
+          // TODO add exception handler (onFailure) or replace "onSuccess" with onComplete to handle failures manually
+          onSuccess(dbResponse) {
+            case JsonResponse(json) =>
+              complete(HttpEntity(ContentTypes.`application/json`, json))
+            case _ => complete("undefined response")
+          }
         }
-      }
     }
   }
 
   val createBooking: Route = path("createBooking") {
     post {
-      entity(as[CreateBookingRequest]) { request =>
-        val dbResponse = dbActors.ask(ref =>
-          CreateBookingCommand(
-            ref,
-            request.companyId,
-            request.masterId,
-            request.startT,
-            request.finishT,
-            request.clientTel
-          )
-        )
+      entity(as[CreateBookingParams]) { params =>
+        val dbResponse = dbActors.ask(ref => CreateBookingCommand(ref, params))
+
         onSuccess(dbResponse) {
-          case StatusCodeResponse(code: Int) =>
+          case SuccessResponse(code: Int) =>
             complete(StatusCode.int2StatusCode(code))
           case _ => complete("undefined response") // TODO
 
@@ -89,19 +72,18 @@ class BookingRoutes(
     get {
       parameters(
         "companyId".as[String],
-        "clientTel".as[String],
         "startT".as[String],
-        "finishT".as[String]
-      ) { (companyId, clientTel, startT, finishT) =>
-        val dbResponse = dbActors.ask(ref =>
-          GetBookingsCommand(ref, companyId, clientTel, startT, finishT)
-        )
-        onSuccess(dbResponse) {
-          case JsonResponse(json) =>
-            complete(HttpEntity(ContentTypes.`application/json`, json))
-          case _ => complete("undefined response") // TODO
+        "finishT".as[String],
+        "clientPhone".as[String]
+      )
+        .as(GetBookingsParams) { params =>
+          val dbResponse = dbActors.ask(ref => GetBookingsCommand(ref, params))
+          onSuccess(dbResponse) {
+            case JsonResponse(json) =>
+              complete(HttpEntity(ContentTypes.`application/json`, json))
+            case _ => complete("undefined response") // TODO
+          }
         }
-      }
     }
   }
 
@@ -111,19 +93,16 @@ class BookingRoutes(
         "bookingId".as[String],
         "status".as[String],
         "message".as[String].optional
-      ) { (bookingId, status, message) =>
-        val msg = message match {
-          case Some(str) => str
-          case None      => ""
+      )
+        .as(EditBookingParams) { params =>
+          val dbResponse =
+            dbActors.ask(ref => EditBookingCommand(ref, params))
+          onSuccess(dbResponse) {
+            case SuccessResponse(code: Int) =>
+              complete(StatusCode.int2StatusCode(code))
+            case _ => complete("undefined response") // TODO
+          }
         }
-        val dbResponse =
-          dbActors.ask(ref => EditBookingCommand(ref, bookingId, status, msg))
-        onSuccess(dbResponse) {
-          case StatusCodeResponse(code: Int) =>
-            complete(StatusCode.int2StatusCode(code))
-          case _ => complete("undefined response") // TODO
-        }
-      }
     }
   }
 
@@ -133,24 +112,30 @@ class BookingRoutes(
         "companyId".as[String],
         "startT".as[String],
         "finishT".as[String]
-      ) { (companyId, startT, finishT) =>
-        val dbResponse = dbActors.ask(ref =>
-          GetCompanyBookingsCommand(ref, companyId, startT, finishT)
-        )
-        onSuccess(dbResponse) {
-          case JsonResponse(json) =>
-            complete(HttpEntity(ContentTypes.`application/json`, json))
-          case _ => complete("undefined response") // TODO
+      )
+        .as(GetCompanyBookingsParams) { params =>
+          val dbResponse =
+            dbActors.ask(ref => GetCompanyBookingsCommand(ref, params))
+          onSuccess(dbResponse) {
+            case JsonResponse(json) =>
+              complete(HttpEntity(ContentTypes.`application/json`, json))
+            case _ => complete("undefined response") // TODO
+          }
         }
-      }
     }
   }
 
   val createWorkingSchedule: Route = path("createWorkingSchedule") {
     post {
-      entity(as[CreateWorkingScheduleRequest]) { request =>
-        ???
-      // TODO
+      entity(as[CreateWorkingScheduleParams]) { params =>
+        val dbResponse =
+          dbActors.ask(ref => CreateWorkingScheduleCommand(ref, params))
+
+        onSuccess(dbResponse) {
+          case SuccessResponse(code) =>
+            complete(StatusCode.int2StatusCode(code))
+          case ExceptionResponse(e) => throw e
+        }
       }
     }
   }
