fb9e60cdae772d58cd76268771a4f610837b4aa8
Vumba798 <alexandrov32649@gmail.com>
2022-08-20 21:08:09 +0300

    added implicit execution context for database actor

--- src/main/scala/server/actors/database_actor/DatabaseActor.scala
+++ src/main/scala/server/actors/database_actor/DatabaseActor.scala
@@ -1,24 +1,23 @@
 package server.actors.database_actor
 
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.Behavior
 import server.actors.database_actor.commands._
 import server.actors.database_actor.handlers._
 
-import scala.concurrent.ExecutionContext
 
 object DatabaseActor {
-
-  // TODO
-  implicit val ec: ExecutionContext = ExecutionContext.global
 
   trait Command // Messages that HttpActor can receive
   trait Response // Messages that will be transmitted to Routes
 
-  def apply(): Behavior[Command] = Behaviors.receiveMessage {
-    case m: BookingCommand   => BookingCommandHandler(m)
-    case m: AuthCommand      => AuthCommandHandler(m)
-    case m: AnalyticsCommand => AnalyticsCommandHandler(m)
-  }
+  def apply(): Behavior[Command] = Behaviors.setup { ctx =>
+    implicit val ec = ctx.executionContext
 
+    Behaviors.receiveMessage {
+      case m: BookingCommand => BookingCommandHandler(m)
+      case m: AuthCommand => AuthCommandHandler(m)
+      case m: AnalyticsCommand => AnalyticsCommandHandler(m)
+    }
+  }
 }
