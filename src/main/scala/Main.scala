32f30f415f4d279f40c21ecf55843faa8208e0da
Vumba798 <alexandrov32649@gmail.com>
2022-08-20 21:12:59 +0300

    moved constants outside of actor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,5 +2,8 @@ import akka.actor.typed.ActorSystem
 import server.actors.server_actor.Server
 
 object Main extends App {
-  ActorSystem(Server(), "booking-system")
+  val host = "localhost"
+  val port = 8080
+  val poolSize = 4
+  ActorSystem(Server(host, port, poolSize), "booking-system")
 }
--- src/main/scala/server/actors/server_actor/Server.scala
+++ src/main/scala/server/actors/server_actor/Server.scala
@@ -1,6 +1,5 @@
 package server.actors.server_actor
 
-import akka.actor.TypedActor.context
 import akka.actor.typed.scaladsl.{Behaviors, Routers}
 import akka.actor.typed.{Behavior, PostStop}
 import akka.http.scaladsl.Http
@@ -18,65 +17,62 @@ object Server {
   private final case class Started(binding: ServerBinding) extends Message
   case object Stop extends Message
 
-  def apply(): Behavior[Message] = Behaviors.setup { ctx =>
-    implicit val system = ctx.system
+  def apply(host: String, port: Int, poolSize: Int): Behavior[Message] =
+    Behaviors.setup { ctx =>
+      implicit val system = ctx.system
 
-    val host = "localhost"
-    val port = 8080
+      // creating a pool of DatabaseActor
+      val pool = Routers.pool(poolSize)(DatabaseActor())
+      val dbActors = ctx.spawn(pool, "worker-pool")
 
-    // creating a pool of DatabaseActor
-    val poolSize = 4
-    val pool = Routers.pool(poolSize)(DatabaseActor())
-    val dbActors = ctx.spawn(pool, "worker-pool")
+      // initializes our routes with giving them ActorRef[DatabaseActor]
+      val routes = new Routes(dbActors).routes
+      val bindingFuture = Http().newServerAt(host, port).bind(routes)
 
-    // initializes our routes with giving them ActorRef[DatabaseActor]
-    val routes = new Routes(dbActors).routes
-    val bindingFuture = Http().newServerAt(host, port).bind(routes)
-
-    // uses callback
-    ctx.pipeToSelf(bindingFuture) {
-      case Success(binding) => Started(binding)
-      case Failure(e)       => StartFailed(e)
-    }
-
-    def running(binding: ServerBinding): Behavior[Message] = {
-      Behaviors
-        .receiveMessagePartial[Message] { case Stop =>
-          ctx.log.info(
-            "Stopping server http://{}:{}/",
-            binding.localAddress.getHostString,
-            binding.localAddress.getPort
-          )
-          Behaviors.stopped
-        }
-        .receiveSignal {
-          // we receive PostStop when the actor was stopped
-          case (_, PostStop) =>
-            binding.unbind()
-            Behaviors.same
-        }
-    }
-
-    def starting(wasStopped: Boolean): Behaviors.Receive[Message] =
-      Behaviors.receiveMessage {
-        case StartFailed(cause) =>
-          throw new RuntimeException("Server failed to start", cause)
-        case Started(binding) =>
-          println("success")
-          ctx.log.info(
-            "Server online at http://{}:{}/",
-            binding.localAddress.getHostString,
-            binding.localAddress.getPort
-          )
-          if (wasStopped) ctx.self ! Stop
-          running(binding)
-        case Stop =>
-          // we got a stop message but haven't completed starting yet,
-          // we cannot stop until starting has completed
-          starting(wasStopped = true)
+      // uses callback
+      ctx.pipeToSelf(bindingFuture) {
+        case Success(binding) => Started(binding)
+        case Failure(e)       => StartFailed(e)
       }
 
-    starting(wasStopped = false)
-  }
+      def running(binding: ServerBinding): Behavior[Message] = {
+        Behaviors
+          .receiveMessagePartial[Message] { case Stop =>
+            ctx.log.info(
+              "Stopping server http://{}:{}/",
+              binding.localAddress.getHostString,
+              binding.localAddress.getPort
+            )
+            Behaviors.stopped
+          }
+          .receiveSignal {
+            // we receive PostStop when the actor was stopped
+            case (_, PostStop) =>
+              binding.unbind()
+              Behaviors.same
+          }
+      }
+
+      def starting(wasStopped: Boolean): Behaviors.Receive[Message] =
+        Behaviors.receiveMessage {
+          case StartFailed(cause) =>
+            throw new RuntimeException("Server failed to start", cause)
+          case Started(binding) =>
+            println("success")
+            ctx.log.info(
+              "Server online at http://{}:{}/",
+              binding.localAddress.getHostString,
+              binding.localAddress.getPort
+            )
+            if (wasStopped) ctx.self ! Stop
+            running(binding)
+          case Stop =>
+            // we got a stop message but haven't completed starting yet,
+            // we cannot stop until starting has completed
+            starting(wasStopped = true)
+        }
+
+      starting(wasStopped = false)
+    }
 
 }
